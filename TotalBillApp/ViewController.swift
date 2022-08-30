ccd0ac8ffabd9cfd93d8743ff5c36bcc59e0392f
Nikolai Maximov <kolkamaximov@gmail.com>
2022-08-30 22:26:18 +0300

    Initial Commit

---
+++ TotalBillApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TotalBillApp
+//
+//  Created by Nikolai Maksimov on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
